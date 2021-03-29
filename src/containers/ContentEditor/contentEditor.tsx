a8f44068f859475e5a664daacb20879f24793257
AbdElMoniem <eslam.abdelmoniem@vodafone.com>
2021-03-29 20:55:01 +0200

    make some changes

--- src/containers/ContentEditor/contentEditor.tsx
+++ src/containers/ContentEditor/contentEditor.tsx
@@ -1,10 +1,18 @@
 import React, { useState } from 'react'
+import { useParams } from 'react-router';
+import { FacebookShareButton } from 'react-share';
 import SunEditor, { buttonList } from 'suneditor-react';
 import 'suneditor/dist/css/suneditor.min.css';
+import { RedOutlineButton } from '../../components/Lables/red-outlline-button';
 import { RedBackgroundButton } from '../../components/Lables/redBackgroundButton';
+import { baseURL } from '../../utils/baseUrl';
 
 const ContentEditor = () => {
     const [content, setContent] = useState<any>();
+    const params = useParams<any>();
+    const campaignId = params.id;
+    const studentId = params.studentId;
+
     return (
         <div>
             <SunEditor
@@ -25,9 +33,15 @@ const ContentEditor = () => {
                 }}
                 showToolbar={true} />
 
-                <div className="text-center" style={{margin: "20px 0px"}}>
-                    <RedBackgroundButton style={{width: "200px"}}>Save Changes</RedBackgroundButton>
+            <div className="container">
+                <div className="d-flex justify-content-center align-items-center" >
+                    <div><RedBackgroundButton style={{ width: "200px" }}>Save Changes</RedBackgroundButton></div>
+
+                    <div><FacebookShareButton className="shareitemStu" id="share-btn" url={`${baseURL}/#/donation?${`name=${''}&`}${`image=${baseURL}/${""}&`}${`campaignId=${campaignId}&`}${`studentId=${studentId}`}`} >
+                        <span style={{ fontWeight: 600, textAlign: "center", width: "100%" }}> <RedOutlineButton style={{ width: "200px" }} >Share Link</RedOutlineButton> </span>
+                    </FacebookShareButton ></div>
                 </div>
+            </div>
         </div>
     )
 
